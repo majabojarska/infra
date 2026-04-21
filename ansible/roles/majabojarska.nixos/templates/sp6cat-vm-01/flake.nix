cfbc3b21d1efcc296689d81daa443d4f5775f57b
Maja Bojarska <majabojarska98@gmail.com>
2026-04-22 01:57:20 +0200

    nixos: sp6cat-vm-01 starting point'

---
+++ ansible/roles/majabojarska.nixos/templates/sp6cat-vm-01/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    agenix.url = "github:ryantm/agenix";
+    copyparty.url = "github:9001/copyparty";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , agenix
+    , copyparty
+    ,
+    }:
+    {
+      nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          agenix.nixosModules.default
+          copyparty.nixosModules.default
+          (
+            { pkgs, ... }:
+            {
+              # add the copyparty overlay to expose the package to the module
+              nixpkgs.overlays = [ copyparty.overlays.default ];
+            }
+          )
+        ];
+      };
+    };
+}
---
+++ ansible/roles/majabojarska.nixos/templates/sp6cat-vm-01/secrets/secrets.nix
@@ -0,0 +1,15 @@
+let
+  # Developers
+  x260 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBW+jmBmPtDv+Bw21i9J4p/pZPdM7SggxBF9FGOWXSM8 majabojarska98@gmail.com";
+  developers = [ x260 ];
+
+  # Systems
+  sp6cat-vm-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBNo31eqosBfzGo91+3ftgHMvOpKWDE3FPY5L/aw/9X1";
+in
+{
+  "ovh.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
+  "copyparty-pass-maja.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
+  "copyparty-pass-baczek.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
+  "fah-token.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
+  "searx-secret-key.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
+}
