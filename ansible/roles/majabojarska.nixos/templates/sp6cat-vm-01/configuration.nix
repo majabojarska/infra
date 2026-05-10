a4e4920d175b6a48bd45a8579221805aa1d223e5
Maja Bojarska <majabojarska98@gmail.com>
2026-05-10 22:35:07 +0200

    anubis: working custom thresholds and difficulty

--- ansible/roles/majabojarska.nixos/templates/sp6cat-vm-01/configuration.nix
+++ ansible/roles/majabojarska.nixos/templates/sp6cat-vm-01/configuration.nix
@@ -412,23 +412,23 @@
               }
             ];
           };
-          middlewares = [ ];
-        };
-
-        blogBeta = {
-          rule = "Host(`beta.majabojarska.dev`)";
-          service = "blog";
-          tls = {
-            certResolver = "letsencrypt";
-            domains = [
-              {
-                main = "beta.majabojarska.dev";
-              }
-            ];
-          };
           middlewares = [ "anubis" ];
         };
 
+        # blogBeta = {
+        #   rule = "Host(`beta.majabojarska.dev`)";
+        #   service = "blog";
+        #   tls = {
+        #     certResolver = "letsencrypt";
+        #     domains = [
+        #       {
+        #         main = "beta.majabojarska.dev";
+        #       }
+        #     ];
+        #   };
+        #   middlewares = [ "anubis" ];
+        # };
+        #
         copyparty = {
           rule = "Host(`copyparty.cloud.majabojarska.dev`)";
           service = "copyparty";
@@ -522,13 +522,88 @@
     };
   };
 
-  services.anubis.instances.blog.settings = {
-    BIND = ":8080";
-    BIND_NETWORK = "tcp";
-    TARGET = " ";
-    REDIRECT_DOMAINS = "beta.majabojarska.dev,majabojarska.dev";
-    PUBLIC_URL = "https://anubis.cloud.majabojarska.dev";
-    COOKIE_DOMAIN = "majabojarska.dev";
+  services.anubis = {
+    defaultOptions = {
+      settings = {
+        WEBMASTER_EMAIL = "majabojarska98@gmail.com";
+        DIFFICULTY = 6;
+      };
+
+      botPolicy = {
+
+        bots = [
+          {
+            import = "(data)/meta/default-config.yaml";
+          }
+        ];
+        status_codes = {
+          CHALLENGE = 403;
+          DENY = 403;
+        };
+
+        store.backend = "memory";
+
+        thresholds = [
+          {
+            name = "no-suspicion";
+            expression.all = [
+              "weight < 10"
+            ];
+            action = "CHALLENGE";
+            challenge = {
+              algorithm = "fast";
+              difficulty = 5;
+            };
+          }
+          {
+            name = "moderate-suspicion";
+            expression.all = [
+              "weight >= 10"
+              "weight < 20"
+            ];
+            action = "CHALLENGE";
+            challenge = {
+              # https://anubis.techaro.lol/docs/admin/configuration/challenges/proof-of-work
+              algorithm = "fast";
+              difficulty = 6;
+            };
+          }
+          {
+            name = "mild-proof-of-work";
+            expression.all = [
+              "weight >= 20"
+              "weight < 30"
+            ];
+            action = "CHALLENGE";
+            challenge = {
+              algorithm = "fast";
+              difficulty = 7;
+            };
+          }
+          {
+            name = "extreme-suspicion";
+            expression = "weight >= 30";
+            action = "CHALLENGE";
+            challenge = {
+              algorithm = "fast";
+              difficulty = 8;
+            };
+          }
+        ];
+      };
+    };
+
+    instances.blog.settings = {
+      BIND = ":8080";
+      BIND_NETWORK = "tcp";
+      TARGET = " ";
+      REDIRECT_DOMAINS = "beta.majabojarska.dev,majabojarska.dev";
+      PUBLIC_URL = "https://anubis.cloud.majabojarska.dev";
+      COOKIE_DOMAIN = "majabojarska.dev";
+      DIFFICULTY = 20;
+      SERVE_ROBOTS_TXT = true;
+    };
+
   };
 
   # Blog
