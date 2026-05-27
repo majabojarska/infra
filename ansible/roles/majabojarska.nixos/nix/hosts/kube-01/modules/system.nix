b04e69e87f6bb519d7e15c6ec40e247772b17cd8
Maja Bojarska <majabojarska98@gmail.com>
2026-05-28 00:12:39 +0200

    kube-01: cron poweroff at 1AM

--- ansible/roles/majabojarska.nixos/nix/hosts/kube-01/modules/system.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/kube-01/modules/system.nix
@@ -55,7 +55,7 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      # "0 1 * * * root shutdown now" # TODO: re-enable after May 4th
+      "0 1 * * * root shutdown now"
     ];
   };
 
