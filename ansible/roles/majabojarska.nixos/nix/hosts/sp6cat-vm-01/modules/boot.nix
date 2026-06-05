c64538863b5fa0997b80ceab6bbd954350b5095c
Maja Bojarska <majabojarska98@gmail.com>
2026-06-05 16:37:15 +0200

    sp6cat-vm-01: initrd kmods aesni_intel, cryptd

--- ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/boot.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/boot.nix
@@ -5,7 +5,10 @@
     blacklistedKernelModules = [ "algif_aead" ];
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-
+    initrd.availableKernelModules = [
+      "aesni_intel"
+      "cryptd"
+    ];
     zfs.forceImportRoot = false;
   };
 }
