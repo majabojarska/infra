1588c4e2dd4b4d653f34a3085b6497ec02ca6731
Maja Bojarska <majabojarska98@gmail.com>
2026-06-27 03:28:02 +0200

    sp6cat-vm-01: copyparty is SOO BACK

--- ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/copyparty.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/copyparty.nix
@@ -1,4 +1,4 @@
-{ config, ...}:
+{ config, pkgs, ... }:
 {
   # https://github.com/9001/copyparty?tab=readme-ov-file#nixos-module
   services.copyparty = {
@@ -10,18 +10,30 @@
     # directly maps to values in the [global] section of the copyparty config.
     # see `copyparty --help` for available options
     settings = {
+      name = "meoooow MEEOOOWWW";
+      name-url = "https://copyparty.cloud.majabojarska.dev";
+      site = "https://copyparty.cloud.majabojarska.dev";
       i = "127.0.0.1";
+
       # use lists to set multiple values
       p = [
         8005
       ];
+
+      # num cores
+      j = 2;
+
       # use booleans to set binary flags
       no-reload = false;
+      #
       # using 'false' will do nothing and omit the value when generating a config
       ignored-flag = false;
       xff-src = "127.0.0.1"; # IP of the reverse proxy
       xff-hdr = "x-forwarded-for"; # HTTP header containing the real client's IP
       rproxy = 1;
+      nid = true;
+      usernames = true;
+      no-dupe = true;
     };
 
     # create users
@@ -45,7 +57,44 @@
       ];
     };
 
+    volumes = {
+      "/public" = {
+        path = "/mnt/wd-usb-hdd/copyparty/public";
+        # see `copyparty --help-accounts` for available options
+        access = {
+          r = "*";
+          rw = [ "@users" ];
+        };
+        flags = {
+          # scan for new files every 60sec
+          scan = 60;
+          # volflag "e2d" enables the uploads database
+          e2d = true;
+        };
+      };
+      "/private" = {
+        # share the contents of "/srv/copyparty"
+        path = "/mnt/wd-usb-hdd/copyparty/private";
+        # see `copyparty --help-accounts` for available options
+        access = {
+          rw = [ "@users" ];
+        };
+        flags = {
+          # "fk" enables filekeys (necessary for upget permission) (4 chars long)
+          fk = 4;
+          # scan for new files every 60sec
+          scan = 60;
+          # volflag "e2d" enables the uploads database
+          e2d = true;
+        };
+      };
+    };
     # you may increase the open file limit for the process
     openFilesLimit = 8192;
+
+    package = pkgs.copyparty.override {
+      # provides exiftool for bin/hooks/image-noexif.py
+      extraPackages = [ pkgs.exiftool ];
+    };
   };
 }
