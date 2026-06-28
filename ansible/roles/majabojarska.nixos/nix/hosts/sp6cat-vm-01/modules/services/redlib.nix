aef7f97b0d4df1d1df18ad49a8429b474f2c9cbf
Maja Bojarska <majabojarska98@gmail.com>
2026-06-28 02:41:16 +0200

    chore(nix): redlib latest custom build

--- ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/redlib.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/redlib.nix
@@ -2,11 +2,41 @@
   pkgs,
   config,
   ...
-}:
-
-{
+}: let
+src = pkgs.fetchFromGitHub {
+   owner = "redlib-org";
+   repo = "redlib";
+   rev = "a4d36e954cf1bd64f209cd8868c5a29edc81b374";
+   hash = "sha256-siyD6A12UALQIV7BMd7zu1TaojleTEYtpxPszuhx1/Y=";
+ };
+ redlib-latest = pkgs.redlib.overrideAttrs (oldAttrs: {
+   version = "0.36.0-unstable-2026-04-24-vendor";
+   inherit src;
+   cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+     inherit src;
+     name = "redlib-0.36.0-unstable-2026-04-24-vendor";
+     hash = "sha256-eO3c7rlFna3DuO31etJ6S4c7NmcvgvIWZ1KVkNIuUqQ=";
+   };
+   nativeBuildInputs =
+     (oldAttrs.nativeBuildInputs or [])
+     ++ (with pkgs; [
+       cmake
+       go
+       perl
+       git
+       rustPlatform.bindgenHook
+     ]);
+   checkFlags =
+     (oldAttrs.checkFlags or [])
+     ++ [
+       "--skip=oauth::tests::test_generic_web_backend"
+       "--skip=oauth::tests::test_mobile_spoof_backend"
+     ];
+ });
+in {
   services.redlib = {
     enable = true;
+    package = redlib-latest;
     address = "127.0.0.1";
     port = config.sp6catVm01.ports.redlib;
     settings = {
