937096198005e6c5ed6b393ad13f1f1297bc83c7
Maja Bojarska <majabojarska98@gmail.com>
2026-07-11 23:23:58 +0200

    refactor(sp6cat-vm-01): grafana add dashboards

--- ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/grafana/grafana.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/grafana/grafana.nix
@@ -37,8 +37,62 @@
           isDefault = true;
         }
       ];
+
+      dashboards.settings = {
+        apiVersion = 1;
+
+        providers = [
+          {
+            name = "default";
+            options.path = "/etc/grafana-dashboards";
+          }
+        ];
+      };
     };
 
+  };
+
+  environment.etc = {
+    "grafana-dashboards/node.json" = {
+      user = "grafana";
+      group = "grafana";
+      source = pkgs.fetchurl {
+        url = "https://grafana.com/api/dashboards/1860/revisions/45/download";
+        sha256 = "sha256-GExrdAnzBtp1Ul13cvcZRbEM6iOtFrXXjEaY6g6lGYY=";
+      };
+    };
+    "grafana-dashboards/smartctl.json" = {
+      user = "grafana";
+      group = "grafana";
+      source = pkgs.fetchurl {
+        url = "https://grafana.com/api/dashboards/22604/revisions/3/download";
+        sha256 = "sha256-gpm/4rzcNv6br8L8cs9O6iWEScojfImWUi1uRXW8UpM=";
+      };
+    };
+    "grafana-dashboards/chrony.json" = {
+      user = "grafana";
+      group = "grafana";
+      source = pkgs.fetchurl {
+        url = "https://grafana.com/api/dashboards/19186/revisions/2/download";
+        sha256 = "sha256-Etm7ZE7ISqh/w/HIxKAUOPJl5Sb+jRUneveuYfLOr0A=";
+      };
+    };
+    "grafana-dashboards/wireguard.json" = {
+      user = "grafana";
+      group = "grafana";
+      source = pkgs.fetchurl {
+        url = "https://grafana.com/api/dashboards/17251/revisions/1/download";
+        sha256 = "sha256-+sCG6rhOnrz/GhRuQ3gr+D8bYyqAuQ7VTmuwo5ZVD48=";
+      };
+    };
+    "grafana-dashboards/zfs.json" = {
+      user = "grafana";
+      group = "grafana";
+      source = pkgs.fetchurl {
+        url = "https://grafana.com/api/dashboards/11364/revisions/2/download";
+        sha256 = "sha256-8pNOiU5zm7dJu9yYuOku19jqcAH7NbrQ8q8x5xdXKjI=";
+      };
+    };
   };
 
   services.traefik.dynamicConfigOptions.http = {
