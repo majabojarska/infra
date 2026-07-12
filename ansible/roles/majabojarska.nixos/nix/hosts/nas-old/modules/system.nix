c47b523fc2ca70abcb6a7a6c88f9296ac0918e06
Maja Bojarska <majabojarska98@gmail.com>
2026-07-13 01:29:38 +0200

    refactor(nix): gc daily older than 1d

--- ansible/roles/majabojarska.nixos/nix/hosts/nas-old/modules/system.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/nas-old/modules/system.nix
@@ -8,8 +8,8 @@
 
   nix.gc = {
     automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+    dates = "daily";
+    options = "--delete-older-than 1d";
   };
 
   nix.optimise = {
