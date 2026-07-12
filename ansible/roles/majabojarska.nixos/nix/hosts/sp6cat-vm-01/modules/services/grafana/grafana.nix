6d7ee91eb8233aa8416c3befea09fb391b6a7edb
Maja Bojarska <majabojarska98@gmail.com>
2026-07-12 15:30:23 +0200

    fix(sp6cat-vm-01): tolerate grafana slow start

--- ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/grafana/grafana.nix
+++ ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/grafana/grafana.nix
@@ -152,9 +152,12 @@
 
   system.preSwitchChecks = {
     grafanaLocalhostNoHttpError = ''
+      # Grafana is sometimes slow to start, so we retry a few times before failing the switch
       ${pkgs.curl}/bin/curl \
         --fail-with-body \
         --silent \
+        --retry 4 \
+        --retry-max-time 15 \
         --show-error \
           http://127.0.0.1:${toString config.sp6catVm01.ports.grafana} \
         >/dev/null
