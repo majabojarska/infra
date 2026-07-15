b55e0cae54baa695c901a8854dfb0d2963427783
Maja Bojarska <majabojarska98@gmail.com>
2026-07-16 00:47:38 +0200

    feat(sp6cat-vm-01): notify on startup/shutdown

---
+++ ansible/roles/majabojarska.nixos/nix/hosts/sp6cat-vm-01/modules/services/notifications.nix
@@ -0,0 +1,19 @@
+{ config, ... }:
+{
+  imports = [
+    ../../../../modules/notifications.nix
+  ];
+
+  age.secrets = {
+    "ntfy-token" = {
+      file = ../../secrets/ntfy-token.age;
+      mode = "0400";
+    };
+  };
+
+  majabojarska.notifications = {
+    enable = true;
+    tokenFile = config.age.secrets."ntfy-token".path;
+    topic = "ntfy.${config.globals.cloudDomain}/power";
+  };
+}
