28e5d6ed03f26bd8852262930a372b37a39e66b4
Maja Bojarska <majabojarska98@gmail.com>
2026-07-16 01:30:52 +0200

    feat(zsh): add default conf

--- ansible/roles/majabojarska.nixos/nix/modules/shell-zsh-fzf.nix
+++ ansible/roles/majabojarska.nixos/nix/modules/shell-zsh-fzf.nix
@@ -1,7 +1,28 @@
 { pkgs, ... }:
 
 {
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      l = "ls -CF";
+      la = "ls -A";
+      ll = "ls -lah";
+    };
+    interactiveShellInit = ''
+      bindkey -e
+
+      HISTFILE=~/.zsh_history
+      HISTSIZE=10000
+      SAVEHIST=10000
+      setopt APPEND_HISTORY
+      setopt SHARE_HISTORY
+      setopt HIST_IGNORE_DUPS
+      setopt HIST_IGNORE_SPACE
+    '';
+  };
 
   users.defaultUserShell = pkgs.zsh;
   users.users.root.shell = pkgs.zsh;
