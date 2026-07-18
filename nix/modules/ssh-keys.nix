0f2a13251d583d8ab8df12ac6c3ac3feef974cde
Maja Bojarska <majabojarska98@gmail.com>
2026-07-18 23:12:13 +0200

    chore: describe ssh public key
    Signed-off-by: Maja Bojarska <majabojarska98@gmail.com>

--- nix/modules/ssh-keys.nix
+++ nix/modules/ssh-keys.nix
@@ -4,7 +4,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBch2rzzVEnWcUbHJctteozpAFyJYXnd8wMC7DWXS9rL" # FP5
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjpxvbmIPz4MXmkbLjwCJN4MJ1Wg6TWgzi4eK/MuyNd majabojarska98@gmail.com" # maja@kube-01
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKAV1AUyhqMQldeCXCH/a2a7vwKxyPMyaqZT9w82elKE"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/sFQGXdCINUUNDr5DMsD/MG7tm4mTisCKF6fwZE1Li" # Vault-ed
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/sFQGXdCINUUNDr5DMsD/MG7tm4mTisCKF6fwZE1Li" # maja personal infra+git 2026-07-18
   ];
   "www-data" = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGZU7nJ9AUBywX6+icIJ3t4NCEoIbnEOzEfGxYYSX5dI" # Bitwarden: SSH key vps-01 deploy-blog
