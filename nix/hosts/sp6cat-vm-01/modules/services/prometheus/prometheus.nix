94694a7eb49fb1bb558e5749a9edccee22d296cb
Maja Bojarska <majabojarska98@gmail.com>
2026-07-18 23:42:44 +0200

    refactor: prom exporter uses fqdn for sp6cat-01
    Signed-off-by: Maja Bojarska <majabojarska98@gmail.com>

--- nix/hosts/sp6cat-vm-01/modules/services/prometheus/prometheus.nix
+++ nix/hosts/sp6cat-vm-01/modules/services/prometheus/prometheus.nix
@@ -49,7 +49,7 @@ in
               "127.0.0.1:${toString config.services.prometheus.exporters.node.port}"
               "kube-01.${config.globals.homeDomain}:${toString config.hosts.kube01.ports.prometheusExporterNode}"
               "pve-01.home.majabojarska.dev:9100"
-              "192.168.81.50:9100" # PVE sp6cat-01 - TODO: figure out why domain resolution doesn't work for this host
+              "sp6cat-01.hswro.majabojarska.dev" # PVE sp6cat-01 - TODO: figure out why domain resolution doesn't work for this host
             ];
           }
         ];
