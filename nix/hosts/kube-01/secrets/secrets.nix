fc895ff46df8cb0007ef2b17023e13390eebcf49
Maja Bojarska <majabojarska98@gmail.com>
2026-08-01 22:35:05 +0200

    chore: kube-01 rm tailscale secret
    Signed-off-by: Maja Bojarska <majabojarska98@gmail.com>

--- nix/hosts/kube-01/secrets/secrets.nix
+++ nix/hosts/kube-01/secrets/secrets.nix
@@ -5,7 +5,6 @@ let
   kube-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBLa2Aq4Zw4RImQ+XHRHxS7gSrKtwan5bEtg6D20k5TT root@nixos";
 in
 {
-  "tailscale-auth-key.age".publicKeys = [ kube-01 ] ++ developers;
   "borgmatic-kubernetes-enc-pass.age".publicKeys = [ kube-01 ] ++ developers;
   "ntfy-token.age".publicKeys = [ kube-01 ] ++ developers;
 }
