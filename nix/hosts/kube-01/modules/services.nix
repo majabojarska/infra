9bd81240f26bb96dcd1f94dadbdd9dcbfda0d2c3
Maja Bojarska <majabojarska98@gmail.com>
2026-08-02 01:00:22 +0200

    chore: note down migration to-dos
    Signed-off-by: Maja Bojarska <majabojarska98@gmail.com>

--- nix/hosts/kube-01/modules/services.nix
+++ nix/hosts/kube-01/modules/services.nix
@@ -5,7 +5,16 @@
     ./services/k3s.nix
     ./services/notifications.nix
     ./services/prometheus-exporters.nix
+
+    # To migrate, once all else is prepared.
     # ./services/immich.nix
     # ./services/paperless.nix
+
+    # To impl.
+    # ./services/jellyfin.nix
+    # ./services/qbittorrent.nix
+    # ./services/change-detection.nix
+    # ./services/audiomuse.nix
+    # ./services/traefik.nix
   ];
 }
