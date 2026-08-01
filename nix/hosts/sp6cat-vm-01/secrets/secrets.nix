5da7e480e99cc7334063914da98ecc8a94fea806
Maja Bojarska <majabojarska98@gmail.com>
2026-08-01 21:17:55 +0200

    chore: rekey sp6cat-vm-01 age secrets
    Signed-off-by: Maja Bojarska <majabojarska98@gmail.com>

--- nix/hosts/sp6cat-vm-01/secrets/secrets.nix
+++ nix/hosts/sp6cat-vm-01/secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  developers = import ../../../modules/keys-developers.nix;
+  developers = import ../../../modules/keys-developers-age.nix;
 
   # Systems
   sp6cat-vm-01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBNo31eqosBfzGo91+3ftgHMvOpKWDE3FPY5L/aw/9X1";
@@ -12,7 +12,6 @@ in
   "wg-baczek-priv-key.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
   "wg-beehive-priv-key.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
   "wd-usb-hdd-key.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
-  "anubis-allow-bearer-token.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
   "grafana-secret-key.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
   "grafana-admin-password.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
   "ntfy-env.age".publicKeys = [ sp6cat-vm-01 ] ++ developers;
--- nix/modules/keys-developers-age.nix
+++ nix/modules/keys-developers-age.nix
@@ -1,8 +1,6 @@
 let
-	maja-old = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBW+jmBmPtDv+Bw21i9J4p/pZPdM7SggxBF9FGOWXSM8 majabojarska98@gmail.com";
-	maja = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJno2gZ9QEGsJx1so1Xfv5SXWm+Kdhl4gxdbmwPJd9+";
+	maja = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII/sFQGXdCINUUNDr5DMsD/MG7tm4mTisCKF6fwZE1Li";
 in
 [
-	maja-old
 	maja
 ]
