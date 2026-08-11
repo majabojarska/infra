9a4d69c570d683c1da0f7be97ecc0e631d3e2922
Maja Bojarska <majabojarska98@gmail.com>
2026-08-11 22:13:53 +0200

    refactor: immich systemd traefik scaffolding
    Signed-off-by: Maja Bojarska <majabojarska98@gmail.com>

--- nix/hosts/kube-01/modules/services/immich.nix
+++ nix/hosts/kube-01/modules/services/immich.nix
@@ -30,4 +30,28 @@ in
     "d ${pathImmich} 0750 immich immich -"
     "d ${pathImmichMedia} 0750 immich immich -"
   ];
+
+  services.traefik.dynamicConfigOptions.http = {
+    routers.immich = {
+      rule = "Host(`immich.${config.globals.homeDomain}`)";
+      service = "immich";
+      # tls = {
+      #   certResolver = "letsencrypt";
+      #   domains = [
+      #     {
+      #       main = "immich.${config.globals.homeDomain}";
+      #     }
+      #   ];
+      # };
+    };
+
+    services.immich.loadBalancer = {
+      servers = [
+        {
+          url = "http://127.0.0.1:${toString config.hosts.kube01.ports.immich}";
+        }
+      ];
+    };
+  };
+
 }
