6bfaa4173373bfbdc18c3c948ac9cf6bc3c1def7
Maja Bojarska <33836570+majabojarska@users.noreply.github.com>
2026-08-20 01:21:07 +0200

    chore(deps): update ghcr.io/valkey-io/valkey:9 docker digest to 70739f8 (#238)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- nix/hosts/kube-01/modules/services/immich.nix
+++ nix/hosts/kube-01/modules/services/immich.nix
@@ -22,7 +22,7 @@ in
 
     containers = {
       "immich-redis" = {
-        image = "ghcr.io/valkey-io/valkey:9@sha256:3acc0687f2a2e1091fae6450d7842dd658c941338cf0a873ddd9e14b9e4ea4dd";
+        image = "ghcr.io/valkey-io/valkey:9@sha256:70739f85ad2ee01a726a965584a0f94895f01b0c60b3cc8b0aeef11eaa6888cf";
         autoStart = true;
 
         extraOptions = [
