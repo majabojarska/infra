9a063002d2538b85840a553e9153ad5d9e3440ba
Maja Bojarska <majabojarska98@gmail.com>
2026-09-03 20:33:02 +0200

    chore: renovate tracks majabojarska/OpenChocolate

--- nix/hosts/sp6cat-vm-01/modules/services/renovate.nix
+++ nix/hosts/sp6cat-vm-01/modules/services/renovate.nix
@@ -45,6 +45,7 @@
         "majabojarska/bitwarden-cli-docker"
         "majabojarska/bitwarden-cli-helm"
         "majabojarska/infra"
+        "majabojarska/OpenChocolate"
       ];
 
       prHourlyLimit = 50;
